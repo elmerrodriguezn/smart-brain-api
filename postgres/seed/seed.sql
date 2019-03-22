2a4c77c02cdfcafee0d738ba829c13719ce3a548
Elmer Rodriguez <elmer.rodriguezn@gmail.com>
2019-03-22 15:56:04 -0700

    adding auth

--- postgres/seed/seed.sql
+++ postgres/seed/seed.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
 
-INSERT into users (name, email, entries, joined) values ('Jessie', 'jessie@gmail.com', 5, '2018-01-01')
-INSERT into login (hash, email) values ('e1671797c52e15f763380b45e841ec32', 'jessie@gmail.com')
+INSERT into users (name, email, entries, joined) values ('a', 'a@a.com', 5, '2018-01-01');
+INSERT into login (hash, email) values ('0cc175b9c0f1b6a831c399e269772661', 'a@a.com');
 
 COMMIT;
